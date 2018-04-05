9f795a7f6f894575a69022c675342bb148d4305c
tienstorm <tienstorm.lg@gmail.com>
2018-04-05 08:11:56 -0500

    UI Finally Done

---
+++ ValidateAddressApp/app/src/main/java/com/example/maikhoi/myapplication/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.maikhoi.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private static final String TAG = "Quick Address Validate";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void sendMessage(View view){
+
+        Log.i(TAG, "sendMessage: I'm a message!");
+    }
+}
